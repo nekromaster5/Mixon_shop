d145d4dd9999409f16a3d0d6a122a63982b0e57c
YokaEnenra <lartaman1337@gmail.com>
2024-01-29 15:15:57 +0200

    Add test view

---
+++ Mixon_shop/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy as _
+from django.views import View
+
+
+class HomePage(View):
+    def get(self, request):
+        return render(request, 'product.html')
