f54ca40866eb8a58ef0cb918c0f75d446921533e
YokaEnenra <lartaman1337@gmail.com>
2024-01-29 16:21:41 +0200

    Add static files Moved src/assers into the static files folder

---
+++ static/src/app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="logo">Mixon</div>
+      <nav>
+        <ul>
+          <li><a href="#">Акции</a></li>
+          <li><a href="#">О нас</a></li>
+          <li><a href="#">Новости</a></li>
+          <li><a href="#">Оплата и доставка</a></li>
+          <li><a href="#">Контакты</a></li>
+        </ul>
+      </nav>
+      <div class="search-bar">
+        <input type="text" placeholder="Поиск..." />
+        <button>Найти</button>
+      </div>
+    </header>
+    <!-- Здесь можете добавить остальное содержимое -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  // Другие компоненты и логика могут быть добавлены сюда
+};
+</script>
+
+<style>
+/* Стили для шапки */
+/* ... */
+</style>
