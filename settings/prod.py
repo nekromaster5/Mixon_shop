df9864590cca3a594cb8bbcdad29939922842c1f
YokaEnenra <lartaman1337@gmail.com>
2024-05-16 15:48:59 +0300

    update settings update allowed hosts

--- settings/prod.py
+++ settings/prod.py
@@ -3,6 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 STATIC_ROOT = BASE_DIR / 'static/'
